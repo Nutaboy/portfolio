3d0b0a758a4b47e57044c5051d6d247dea7c3948
Nutaboy <fabian.brunner@windowslive.com>
2021-07-23 13:47:46 +0200

    changed back JS variant

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,39 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-
-  const codeBlock = document.getElementById('code');
-  const copyButton = document.getElementById('copy-button');
-  const copySuccess = document.getElementById('copy-success');
-
-  const copyTextHandler = () => {
-    const text = codeBlock.innerText;
-
-    // first version - document.execCommand('copy');
-    // var element = document.createElement('textarea');
-    // document.body.appendChild(element);
-    // element.value = text;
-    // element.select();
-    // document.execCommand('copy');
-    // document.body.removeChild(element);
-
-    // copySuccess.classList.add('show-message');
-    // setTimeout(() => {
-    //   copySuccess.classList.remove('show-message');
-    // }, 2500);
-
-    //   second version - Clipboard API
-    navigator.clipboard.writeText(text).then(
-      () => {
-        copySuccess.classList.add('show-message');
-        setTimeout(() => {
-          copySuccess.classList.remove('show-message');
-        }, 2500);
-      },
-      () => {
-        console.log('Error writing to the clipboard');
+function createCopyButton(codeBracket) {
+    const button = document.createElement("button");
+    button.className = "copy-code-button";
+    button.type = "button";
+    button.innerText = "Copy";
+    button.addEventListener("click", () => copyCodeToClipboard(button, codeBracket));
+    addCopyButtonToDom(button, codeBracket);
+  }
+  
+  async function copyCodeToClipboard(button, codeBracket) {
+    const codeToCopy = codeBracket.innerText;
+    try {
+      result = await navigator.permissions.query({ name: "clipboard-write" });
+      if (result.state == "granted" || result.state == "prompt") {
+        await navigator.clipboard.writeText(codeToCopy);
+      } else {
+        copyCodeBlockExecCommand(codeToCopy, codeBracket);
       }
-    );
-  };
-
-  copyButton.addEventListener('click', copyTextHandler);
-});
+    } catch (_) {
+      copyCodeBlockExecCommand(codeToCopy, codeBracket);
+    }
+    finally {
+      codeWasCopied(button);
+    }
+  }
+  
+  function copyCodeBlockExecCommand(codeToCopy, codeBracket) {
+    const textArea = document.createElement("textArea");
+    textArea.contentEditable = 'true'
+    textArea.readOnly = 'false'
+    textArea.className = "copyable-text-area";
+    textArea.value = codeToCopy;
+    codeBracket.insertBefore(textArea, codeBracket.firstChild);
+    const range = document.createRange()
+    range.selectNodeContents(textArea)
+    const sel = window.getSelection()
+    sel.removeAllRanges()
+    sel.addRange(range)
+    textArea.setSelectionRange(0, 999999)
+    document.execCommand("copy");
+    codeBracket.removeChild(textArea);
+  }
+  
+  function codeWasCopied(button) {
+    button.blur();
+    button.innerText = "Copied!";
+    setTimeout(function() {
+      button.innerText = "Copy";
+    }, 2000);
+  }
+  
+  function addCopyButtonToDom(button, codeBracket) {
+    codeBracket.appendChild(button);
+    const wrapper = document.createElement("div");
+    wrapper.className = "highlight-wrapper";
+    codeBracket.parentNode.insertBefore(wrapper, codeBracket);
+    wrapper.appendChild(codeBracket);
+  }
+  
+  document.querySelectorAll("code")
+    .forEach(codeBracket => createCopyButton(codeBracket));
