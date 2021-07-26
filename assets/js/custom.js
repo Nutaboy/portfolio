684278a5982ae3706147c8d266a7705b72111ddb
Nutaboy <fabian.brunner@windowslive.com>
2021-07-26 15:22:46 +0200

    reworked js for label v4

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -62,7 +62,7 @@ function addCopyButtonToDom(button, codeBlock) {
 }
 
 function addCodeLabelToDom(label, codeBlock) {
-  codeBlock.parentNode.insertBefore(label, codeBlock);
+  codeBlock.parentNode.insertAdjacentHTML("afterbegin", label);
 }
 
 document.querySelectorAll("pre > code:not(.language-mermaid)")
